bfb4f36ac82494a9c796d4823298d2dbbd60d3ab
techterror12 <techterror12@gmail.com>
2018-04-30 18:07:07 -0400

    edits no more auth.go moved to main.go

--- Thy-Twitch-Bot/main.go
+++ Thy-Twitch-Bot/main.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"github.com/ThyCorp/Twitch-Stuff/Thy-Twitch-Bot/pkg/bot"
+	"github.com/ThyCorp/Twitch-Stuff/Thy-Twitch-Bot/pkg/open"
+	"github.com/ThyCorp/Twitch-Stuff/Thy-Twitch-Bot/pkg/twitch"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -50,17 +52,19 @@ func runBot() {
 // PassFinder finds twitch pass word and stores it in txt file for later use
 func PassFinder() {
 	filename := "twitch_pass.txt"
-	in := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter Pass")
-	u1, err := in.ReadString('\n')
-	if err != nil {
-		fmt.Println("Somthing Went Wrong Rerun")
-		os.Exit(1)
-	}
+	pass := auth()
 	file, err := os.Create(filename)
 	if err != nil {
 		fmt.Println("error At File Create")
 		os.Exit(1)
 	}
 	file.WriteString(u1)
+}
+
+func auth() string {
+	out := twitch.New("avq1j7x3f8s9dzesyq67s7nfa8hccm", "a1rrg32a45m8nsx3pfukudymcvgf0x", "http://localhost", "viewing_activity_read")
+	open.Open("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=avq1j7x3f8s9dzesyq67s7nfa8hccm&redirect_uri=http://localhost&scope=channel_feed_read+channel_feed_edit&state=a1rrg32a45m8nsx3pfukudymcvgf0x")
+	in := bufio.NewReader()
+	fmt.Println("Enter In Auth-Token")
+	return in.ReadString()
 }
